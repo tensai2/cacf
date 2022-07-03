e3cc0dca40a8173a56cbcce72748c003c2bf061f
= <suttapak.matee@gmail.com>
2022-07-03 14:01:51 +0700

    hot fix message repo

--- repositories/message.go
+++ repositories/message.go
@@ -1,8 +1,10 @@
 package repositories
 
-type Message interface {
-	Get(permission string) (*Message, error)
-	Create(message Message) error
-	Update(message Message) error
-	Delete(message Message) error
+import "github.com/suttapak/cacf/models"
+
+type MessageRepository interface {
+	Get(permission string) (*models.Message, error)
+	Create(message models.Message) error
+	Update(message models.Message) error
+	Delete(message models.Message) error
 }
